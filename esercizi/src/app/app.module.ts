5b22a48e59f0e675f66832f314735a43c058c6f4
gabrielaolteanu <gabrielaolteanu15@gmail.com>
2022-05-03 12:16:26 +0200

    d

--- esercizi/src/app/app.module.ts
+++ esercizi/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserContainerComponent } from './components/user-container/user-container.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserSingleComponent } from './components/user-single/user-single.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { OrderContainerComponent } from './components/order-container/order-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserContainerComponent,
+    UserListComponent,
+    UserSingleComponent,
+    UserDetailComponent,
+    OrderContainerComponent
   ],
   imports: [
     BrowserModule,
