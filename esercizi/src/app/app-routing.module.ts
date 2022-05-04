0033e2d0ee083ad773e90d025241b410174bc819
gabrielaolteanu <gabrielaolteanu15@gmail.com>
2022-05-04 15:40:35 +0200

    ex-8

--- esercizi/src/app/app-routing.module.ts
+++ esercizi/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderContainerComponent } from './components/order-container/order-container.component';
+import { UserContainerComponent } from './components/user-container/user-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "users", component:UserContainerComponent
+  }, {
+    path:"orders", component: OrderContainerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
